84c04657915dca752cf225d4cab9aac436ff935b
Scott Mangiapane <scottmangiapane@gmail.com>
2016-01-30 07:24:09 -0500

    UI tweaks
    Changed general GUI.

--- app/src/main/java/com/cactuslabs/boilerbites/MainActivity.java
+++ app/src/main/java/com/cactuslabs/boilerbites/MainActivity.java
@@ -1,12 +1,10 @@
 package com.cactuslabs.boilerbites;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,11 +13,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setSupportActionBar((Toolbar) findViewById(R.id.app_bar));
         new MainView(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+//                startActivity(new Intent(this, SettingsActivity.class));
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     public void refresh() {
         setContentView(R.layout.activity_main);
+        setSupportActionBar((Toolbar) findViewById(R.id.app_bar));
         new MainView(this);
     }
 }

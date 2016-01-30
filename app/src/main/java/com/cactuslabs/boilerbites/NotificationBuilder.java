79a54ff1cc31f2491b06b9446d17dcdfe672e65d
Scott Mangiapane <scottmangiapane@gmail.com>
2016-01-30 10:22:59 -0500

    Updated

--- app/src/main/java/com/cactuslabs/boilerbites/NotificationBuilder.java
+++ app/src/main/java/com/cactuslabs/boilerbites/NotificationBuilder.java
@@ -13,21 +13,21 @@ public class NotificationBuilder extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         Preferences preferences = new Preferences(context);
         String[] keywords = preferences.getData().split("\t");
-        //start
-        NotificationCompat.Builder builder =
-                new NotificationCompat.Builder(context)
-                        .setSmallIcon(R.drawable.ic_local_dining_white_24dp)
-                        .setContentTitle("BoilerBites")
-                        .setContentText("Your favorite foods are being served.");
-        TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
-        stackBuilder.addParentStack(OverviewActivity.class);
-        stackBuilder.addNextIntent(new Intent(context, OverviewActivity.class));
-        PendingIntent resultPendingIntent =
-                stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
-        builder.setContentIntent(resultPendingIntent);
-        NotificationManager notificationManager =
-                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-        notificationManager.notify((int) (Math.random() * 10000), builder.build());
-        //end
+        if () { /*true if favorite foods (keywords) are being served*/
+            NotificationCompat.Builder builder =
+                    new NotificationCompat.Builder(context)
+                            .setSmallIcon(R.drawable.ic_local_dining_white_24dp)
+                            .setContentTitle("BoilerBites")
+                            .setContentText("Your favorite foods are being served.");
+            TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+            stackBuilder.addParentStack(OverviewActivity.class);
+            stackBuilder.addNextIntent(new Intent(context, OverviewActivity.class));
+            PendingIntent resultPendingIntent =
+                    stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+            builder.setContentIntent(resultPendingIntent);
+            NotificationManager notificationManager =
+                    (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+            notificationManager.notify((int) (Math.random() * 10000), builder.build());
+        }
     }
 }
